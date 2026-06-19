8f6245bd3fe39a4c146f2f53cf21b8d553755929
Sylvie00 <taylorkellogg540@gmail.com>
2026-06-19 00:57:48 -0500

    more deps

--- build_files/build-xfce.sh
+++ build_files/build-xfce.sh
@@ -9,7 +9,7 @@ XFDESKTOP_REF="${XFDESKTOP_REF:-master}"
 
 #deps
 dnf -y builddep libxfce4windowing libxfce4ui xfce4-panel xfdesktop
-dnf -y install git meson ninja-build gtk-layer-shell-devel make cmake gstreamer1 gstreamer1-devel gstreamer1-plugins-base-devel
+dnf -y install git meson ninja-build gtk-layer-shell-devel make cmake gstreamer1 gstreamer1-devel gstreamer1-plugins-base-devel gstreamer1-plugins-good-gtk
 
 cd /tmp
 
