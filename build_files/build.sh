6b9db4c3a8fe5cb72aa0030f3be87ac1c95bc243
Sylvie00 <taylorkellogg540@gmail.com>
2026-06-19 19:40:05 -0500

    matugen

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,7 @@ dnf install -y niri --setopt=install_weak_deps=False
 
 #misc utils
 dnf install -y tailscale zoxide fastfetch xfce4-terminal \
-    sddm qt6ct fuzzel swww
+    sddm qt6ct fuzzel swww matugen
 
 #gamerslop
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
