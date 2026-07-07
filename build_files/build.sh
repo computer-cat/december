bca615a11d1da367ad55feb128d1121e41292da1
Sylvie00 <taylorkellogg540@gmail.com>
2026-07-06 22:44:57 -0500

    meow

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,7 +20,7 @@ dnf install -y noctalia-git noctalia-greeter
 
 #misc utils
 dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-dnf install -y tailscale zoxide fastfetch foot micro nautilus bash-completions
+dnf install -y tailscale zoxide fastfetch foot micro nautilus
 
 #gamerslop
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
