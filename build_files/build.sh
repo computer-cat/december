045b1913ed1a7946186c17a1c6c8a0a44739223e
Sylvie00 <taylorkellogg540@gmail.com>
2026-07-06 22:42:07 -0500

    woops

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,9 +6,13 @@ set -ouex pipefail
 cp -avf "/ctx/system_files"/. /
 dnf install -y dnf5-plugins
 
+#essentials
+dnf install -y git distrobox flatpak
+
 #wm
 dnf copr enable -y yalter/niri
 dnf install -y niri --setopt=install_weak_deps=False
+dnf install -y xdg-desktop-portal-gtk xdg-desktop-portal-gnome gnome-keyring
 
 #shell
 dnf copr enable -y lionheartp/Hyprland
@@ -16,7 +20,7 @@ dnf install -y noctalia-git noctalia-greeter
 
 #misc utils
 dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-dnf install -y tailscale zoxide fastfetch foot micro
+dnf install -y tailscale zoxide fastfetch foot micro nautilus bash-completions
 
 #gamerslop
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
