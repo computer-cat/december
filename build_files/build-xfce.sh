217896b7d3042a03188a2a8c75417ba1d4957747
Sylvie00 <taylorkellogg540@gmail.com>
2026-07-18 18:35:59 -0500

    attempt building xfwl

--- build_files/build-xfce.sh
+++ build_files/build-xfce.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+#xfce wayland session installer
+
 set -euxo pipefail
 
+mkdir -p /var/roothome
+
 #deps
-dnf -y builddep libxfce4windowing libxfce4ui xfce4-panel xfdesktop
+dnf -y builddep libxfce4windowing libxfce4ui xfce4-panel xfdesktop xfconf
 dnf -y install git meson ninja-build gtk-layer-shell-devel make cmake \
-    gstreamer1 gstreamer1-devel gstreamer1-plugins-base-devel \
-    gstreamer1-plugins-good-gtk
+  gstreamer1 gstreamer1-devel gstreamer1-plugins-base-devel \
+  gstreamer1-plugins-good-gtk \
+  rust cargo \
+  gettext libdisplay-info-devel libdrm-devel mesa-libgbm-devel \
+  gtk3-devel libinput-devel pixman-devel libseat-devel \
+  systemd-devel libxfce4ui-devel xfconf-devel libxkbcommon-devel \
+  xorg-x11-server-Xwayland
 
 cd /tmp
 
@@ -22,6 +31,7 @@ cd /tmp
 #libxfce4ui
 git clone --depth 1 https://gitlab.xfce.org/xfce/libxfce4ui.git
 cd libxfce4ui
+git submodule update --init --recursive --depth 1
 meson setup build --prefix=/usr
 meson compile -C build
 meson install -C build
@@ -36,11 +46,41 @@ meson install -C build
 cd /tmp
 
 #xfdesktop
-#git clone --depth 1 https://gitlab.xfce.org/xfce/xfdesktop.git
-#cd xfdesktop
-#meson setup build --prefix=/usr -Dwayland=enabled
-#meson compile -C build
-#meson install -C build
+git clone --depth 1 https://gitlab.xfce.org/xfce/xfdesktop.git
+cd xfdesktop
+git submodule update --init --recursive --depth 1
+meson setup build --prefix=/usr -Dwayland=enabled
+meson compile -C build
+meson install -C build
+cd /tmp
+
+#xfconf
+git clone --depth 1 https://gitlab.xfce.org/xfce/xfconf.git
+cd xfconf
+git submodule update --init --recursive --depth 1
+meson setup build
+meson compile -C build
+meson install -C build
+cd /tmp
 
 #xfce4-settings
-dnf -y install xfce4-settings thunar-archive-plugin xfce4-pulseaudio-plugin
+git clone --depth 1 https://gitlab.xfce.org/xfce/xfce4-settings.git
+cd xfce4-settings
+git submodule update --init --recursive --depth 1
+meson setup build
+meson compile -C build
+meson install -C build
+cd /tmp
+
+#xfwl4
+git clone --depth 1 https://gitlab.xfce.org/xfce/xfwl4.git
+cd xfwl4
+git submodule update --init --recursive --depth 1
+meson setup --wipe build
+meson compile -C build
+meson install -C build
+cd /tmp
+
+
+#extras
+dnf -y install thunar-archive-plugin xfce4-pulseaudio-plugin
--- build_files/build.sh
+++ build_files/build.sh
@@ -7,30 +7,11 @@ cp -avf "/ctx/system_files"/. /
 
 dnf install -y dnf5-plugins
 
-#wm
-dnf copr enable -y yalter/niri
-dnf install -y niri --setopt=install_weak_deps=False
-
-
 #misc utils
 dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 dnf install -y tailscale zoxide fastfetch xfce4-terminal \
-    qt6ct swaybg matugen foot swaylock \
-    xdg-desktop-portal-gtk xdg-desktop-portal-gnome \
-    gnome-keyring fuzzel micro awww tuigreet polkit-kde \
-    pavucontrol chezmoi
+    qt6ct matugen micro pavucontrol
 
 #gamerslop
-dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-dnf install -y steam mangohud gamescope
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+#dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+#dnf install -y steam mangohud gamescope
