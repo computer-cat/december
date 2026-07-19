41f2089bedef2bda62a54086fb7a3579720d221c
Sylvie00 <taylorkellogg540@gmail.com>
2026-07-18 20:32:50 -0500

    add lightdm

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ dnf install -y dnf5-plugins
 #misc utils
 dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 dnf install -y tailscale zoxide fastfetch xfce4-terminal \
-    qt6ct matugen micro pavucontrol
+    qt6ct matugen micro pavucontrol lightdm lightdm-gtk
 
 #gamerslop
 #dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
